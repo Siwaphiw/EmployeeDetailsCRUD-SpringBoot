d03b07ffd34c66ef50d24a121d0d2700f48c2a11
Siwaphiwe_Mbelu <78079407+Siwaphiw@users.noreply.github.com>
2023-02-03 07:53:30 +0200

    Delete Spring-Boot-CRUD-App directory

--- Spring-Boot-CRUD-App/src/main/java/com/Siwaphiwe/Spring/Boot/CRUD/App/Dao/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.Siwaphiwe.Spring.Boot.CRUD.App.Dao;
-
-import com.Siwaphiwe.Spring.Boot.CRUD.App.Models.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-}
